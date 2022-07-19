a8edf6672cf4540e471ce32501e35e366beb3c2f
Allen Liu <liuzhexian11235@gmail.com>
2022-07-19 12:36:21 -0700

    done

--- src/Client.java
+++ src/Client.java
@@ -29,12 +29,13 @@ public class Client {
 			boolean lostBefore = false;
 			int adjustWindow = 0; // 0 for doubles, 1 for half, 2 for +1;
 			Random rand = new Random();
+			List<Integer> windowSizes = new ArrayList<>();
 
-			while (segCount < 100) {
+			while (segCount < 10000) {
 				System.out.println("\n ========================= \n");
 				if (ack == segment) {
 					for (int i = 0; i < windowSize; i++) {
-						int send = rand.nextInt(10);
+						int send = rand.nextInt(1000);
 						if (send == 0) {
 							System.out.println(segment + " will be lost");
 						}
@@ -42,20 +43,18 @@ public class Client {
 
 						if (send != 0) {
 							output.writeInt(segment);
+							ack = input.readInt();
+							System.out.println("ack: " + ack);
+							segCount++;
 						}
-						segCount++;
+
 						// System.out.println("sending seg: " + segment);
 
-						if (segCount >= 100) {
-							continue;
+						if (segCount >= 10000) {
+							break;
 						} else {
 							segment = getNextSeg(segment);
 						}
-						if (send != 0) {
-							ack = input.readInt();
-							System.out.println("ack: " + ack);
-						}
-						// System.out.println("ack: " + ack);
 
 					}
 					adjustWindow = lostBefore ? 2 : 0;
@@ -79,15 +78,20 @@ public class Client {
 						windowSize /= 2;
 					}
 					System.out.println("halfed windowSize = " + windowSize);
-				} else {
+				} else if (adjustWindow == 2 && windowSize < Math.pow(2, 16)) {
 					windowSize++;
 					System.out.println("windowSize + 1 = " + windowSize);
 				}
-				// get the ACK number from the server
+				windowSizes.add(windowSize);
 			}
 			System.out.println("total seg sent: " + segCount);
 			System.out.println("resent: " + resentCount);
+			windowSizes.add(0, 1);
+			for (Integer i : windowSizes) {
+				System.out.println(i);
+			}
 			output.writeInt(-1);
+			output.writeInt(resentCount);
 			input.close();
 			output.close();
 			socket.close();
--- src/Server.java
+++ src/Server.java
@@ -30,40 +30,54 @@ public class Server {
                     // notify the client that the connection is successful
                     output.writeUTF("Success");
                     int ack = 1;
-                    int segRecCount = 0;
+                    // int segRecCount = 0;
                     TreeSet<Integer> buffer = new TreeSet<>(); // store the correct segment
+                    // List<Double> goodPutList = new ArrayList<>();
+                    int resentCount = 0;
+
                     int seg = input.readInt();
                     while (seg != -1) {
-                        System.out.println(seg);
+                        System.out.println("seg received: " + seg);
                         // buffer the segment
-                        if (seg == 1) {
-                            buffer.clear();
-                            System.out.println("wrap around, buffer cleared");
-                        }
-                        if (buffer.add(seg)) {
-                            segRecCount++;
-                        }
-                        while (seg <= buffer.last() && buffer.contains(ack)) {
 
+                        buffer.add(seg);
+
+                        while (buffer.contains(ack) && seg <= buffer.last()) {
+                            if (getNextSeg(ack) == 1) {
+                                buffer.clear();
+                                buffer.add(1);
+                                System.out.println("wrap around, buffer cleared");
+                            }
                             ack = getNextSeg(ack);
                         }
 
                         output.writeInt(ack);
+
                         seg = input.readInt();
                         // calculate good-put periodically
-                        // if (segRecCount == 1000) {
+                        // if (segRecCount % 1000 == 0) {
                         // output.writeInt(-2);
-                        // int sentCount = input.readInt();
-                        // double res = 1000 / (double) sentCount;
+                        // resendCount = input.readInt();
+                        // double res = 1000.0 / (resendCount + 1000);
                         // System.out.println("The good-put of the last 1000 segments received = " +
                         // res);
+                        // goodPutList.add(res);
                         // }
 
                     }
-                    for (Integer i : buffer) {
-                        System.out.println(i);
-                    }
-                    System.out.println("total seg received: " + segRecCount);
+
+                    resentCount = input.readInt();
+                    System.out.println("\n ========================= \n");
+                    double res = 5000.0 / (5000.0 + resentCount);
+                    System.out.println("Total number of segment resent: " + resentCount);
+                    System.out.println("Avg good-put: " + res);
+                    // System.out.println("total seg received: " + segRecCount);
+                    // double sum = 0;
+                    // for (double d : goodPutList) {
+                    // sum += d;
+                    // }
+                    // System.out.println("resendCount: " + resendCount);
+                    // System.out.println("Avg good-put: " + sum / goodPutList.size());
                 } else {
                     System.out.println("Connection failed, please try again.");
                 }
